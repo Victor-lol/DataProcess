ce9f96f9363a55194eebfcf39c44fb91f2ae4578
Victor-lol <qingsheng0524@gmail.com>
2025-01-10 23:55:22 -0500

    add aqua

--- download.py
+++ download.py
@@ -2,8 +2,8 @@
 from datasets import load_dataset
 
 
-dataset_name = 'shangzhu/ChemQA-lite'
-save_path = './Data/ChemQA'
+dataset_name = 'deepmind/aqua_rat'
+save_path = './Data/AQRA-RAT'
 
 dataset = load_dataset(dataset_name, ignore_verifications=True)
 dataset.save_to_disk(save_path)
--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,64 @@
 import json 
 from datasets import load_from_disk
 import random 
+import csv
+from tqdm import tqdm
+import os
 
-path = ''
+path = 'Data/AQRA-RAT'
 dataset = load_from_disk(path)
-print(dataset)
+
+# Save the list of dictionaries to a CSV file
+def save_to_csv(data, file_name):
+    # Extract the keys for the CSV header from the first dictionary
+    keys = data[0].keys()
+    
+    with open(file_name, mode='w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=keys)
+        
+        # Write the header
+        writer.writeheader()
+        
+        # Write the data rows
+        writer.writerows(data)
+
+def process_AQUA(dataset):
+
+    train = dataset['train']
+    test = dataset['test']
+    train_list, test_list = [], []
+    root = './ProcessedData/AQUA'
+
+    os.makedirs(root, exist_ok=True)
+    
+    for row in tqdm(train):
+        question = row['question']
+        label = row['correct']
+        inputs = row['options']
+
+        train_list.append({
+            'ContextString': inputs, 
+            'Behavior': question, 
+            'answer': label
+        })
+    for row in tqdm(test):
+        question = row['question']
+        label = row['correct']
+        inputs = row['options']
+
+        test_list.append({
+            'ContextString': inputs, 
+            'Behavior': question, 
+            'answer': label
+        })
+    
+    train_list = random.sample(train_list, k=400)
+    test_list = random.sample(test_list, k=400)
+
+
+    save_to_csv(train_list, os.path.join(root, 'train_aqua.csv'))
+    save_to_csv(test_list, os.path.join(root, 'train_aqua.csv'))
+    
+
+
+process_AQUA(dataset)
