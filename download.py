5473a61f1c72b0850892b89682a4ca14bf006002
Victor-lol <qingsheng0524@gmail.com>
2025-01-25 23:51:43 -0500

    pubmedqa

--- download.py
+++ download.py
@@ -2,8 +2,9 @@
 from datasets import load_dataset
 
 
-dataset_name = 'deepmind/aqua_rat'
-save_path = './Data/AQRA-RAT'
+dataset_name = 'llamafactory/PubMedQA'
+save_path = './Data/PubMedQA'
 
 dataset = load_dataset(dataset_name, ignore_verifications=True)
+print(len(dataset))
 dataset.save_to_disk(save_path)
--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,8 @@ import csv
 from tqdm import tqdm
 import os
 
-path = 'Data/AQRA-RAT'
+# path = 'Data/AQRA-RAT'
+path = 'Data/PubMedQA'
 dataset = load_from_disk(path)
 
 # Save the list of dictionaries to a CSV file
@@ -22,6 +23,41 @@ def save_to_csv(data, file_name):
         
         # Write the data rows
         writer.writerows(data)
+
+def process_pubmed(dataset):
+    train = dataset['train']
+    test = dataset['test']
+    train_list, test_list = [], []
+    root = './ProcessedData/PubMedQA'
+
+    os.makedirs(root, exist_ok=True)
+    
+    for row in tqdm(train):
+        question = row['input']
+        label = row['output']
+        inputs = row['instruction']
+
+        train_list.append({
+            'ContextString': inputs, 
+            'Behavior': question, 
+            'answer': label
+        })
+    for row in tqdm(test):
+        question = row['input']
+        label = row['output']
+        inputs = row['instruction']
+
+        test_list.append({
+            'ContextString': inputs, 
+            'Behavior': question, 
+            'answer': label
+        })
+    
+    train_list = random.sample(train_list, k=400)
+    test_list = random.sample(test_list, k=400)
+    
+    save_to_csv(train_list, os.path.join(root, 'train_aqua.csv'))
+    save_to_csv(test_list, os.path.join(root, 'train_aqua.csv'))
 
 def process_AQUA(dataset):
 
@@ -62,4 +98,5 @@ def process_AQUA(dataset):
     
 
 
-process_AQUA(dataset)
+# process_AQUA(dataset)
+process_pubmed(dataset)
